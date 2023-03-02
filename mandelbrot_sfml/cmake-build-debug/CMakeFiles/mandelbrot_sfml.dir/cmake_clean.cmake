2e516a539cbf0c1a72fd145af27b6b3545c8abbf
aaandreyyyy <126810772+aaandreyyyy@users.noreply.github.com>
2023-03-02 20:39:50 +0300

    Add files via upload

---
+++ mandelbrot_sfml/cmake-build-debug/CMakeFiles/mandelbrot_sfml.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mandelbrot_sfml.dir/main.cpp.o"
+  "mandelbrot_sfml"
+  "mandelbrot_sfml.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mandelbrot_sfml.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
